bdcd8aac4d2f5b3a5e5c6215faa6d4ab005a2547
Oleksii Semilietov <spylik@gmail.com>
2024-05-12 10:51:36 +0700

    return proper mine-type

--- src/job_processor_handler.erl
+++ src/job_processor_handler.erl
@@ -16,7 +16,7 @@ init(Req, State) ->
     ReplyMode = reply_mode(Req),
     case job_processor:process_tasks(Body, ReplyMode) of
         {ok, Result} ->
-            Req2 = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, Result, Req),
+            Req2 = cowboy_req:reply(200, #{<<"content-type">> => pick_mine_type(ReplyMode)}, Result, Req),
             {ok, Req2, State};
         {error, _Reason} ->
             Req2 = cowboy_req:reply(400, #{<<"content-type">> => <<"application/json">>}, ?STANDARD_ERR, Req),
@@ -34,5 +34,12 @@ reply_mode(Req) ->
         <<"sorted">> -> sorted;
         <<"bash_script">> -> bash_script
     end.
+
+-spec pick_mine_type(ReplyMode) -> Result when
+    ReplyMode   :: reply_mode(),
+    Result      :: binary().
+
+pick_mine_type(sorted) -> <<"application/json">>;
+pick_mine_type(bash_script) -> <<"text/x-shellscript">>.
 
 % ------------------------ end of Private helpers ------------------------------
--- test/cs_challenge_app_tests.erl
+++ test/cs_challenge_app_tests.erl
@@ -21,10 +21,13 @@ sort_tasks_test_() ->
 
                         ExpectedJsonReply = "{\"tasks\":[{\"command\":\"touch /tmp/file1\",\"name\":\"task-1\"},{\"command\":\"echo 'Hello World!' > /tmp/file1\",\"name\":\"task-3\"},{\"command\":\"cat /tmp/file1\",\"name\":\"task-2\"},{\"command\":\"rm /tmp/file1\",\"name\":\"task-4\"}]}",
                         ExpectedBashScript = "#!/usr/bin/env bash\ntouch /tmp/file1\necho 'Hello World!' > /tmp/file1\ncat /tmp/file1\nrm /tmp/file1\n",
-                        {ok, {{"HTTP/1.1",200,"OK"}, _HeadersSorted, ResponseForSorted}} = httpc:request(post, {?SORTED_URL, Headers, "application/json", InputJson}, [], []),
+                        {ok, {{"HTTP/1.1",200,"OK"}, HeadersSorted, ResponseForSorted}} = httpc:request(post, {?SORTED_URL, Headers, "application/json", InputJson}, [], []),
+                        ?assert(lists:member({"content-type","application/json"}, HeadersSorted)),
                         ?assertEqual(ExpectedJsonReply, ResponseForSorted),
 
-                        {ok, {{"HTTP/1.1",200,"OK"}, _HeadersBash, ResponseForBash}} = httpc:request(post, {?BASH_SCRIPT_URL, Headers, "application/json", InputJson}, [], []),
+
+                        {ok, {{"HTTP/1.1",200,"OK"}, HeadersBash, ResponseForBash}} = httpc:request(post, {?BASH_SCRIPT_URL, Headers, "application/json", InputJson}, [], []),
+                        ?assert(lists:member({"content-type","text/x-shellscript"}, HeadersBash)),
                         ?assertEqual(ExpectedBashScript, ResponseForBash)
                     end
                 }

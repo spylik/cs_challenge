628dac0e3eb455f5e5dd606d03c50c567086352b
Oleksii Semilietov <spylik@gmail.com>
2024-05-16 18:44:22 +0700

    handle loops

--- src/job_processor.erl
+++ src/job_processor.erl
@@ -39,25 +39,32 @@ process_tasks(JsonAsBinary, Mode) ->
     SortedTasks :: [task()],
     Result      :: {ok, binary()} | {error, term()}.
 
-produce_output(sorted, SortedTasks) ->
+produce_output(sorted, SortedTasks) when is_list(SortedTasks) ->
     {ok, thoas:encode(#{tasks => SortedTasks})};
 
-produce_output(bash_script, SortedTasks) ->
+produce_output(bash_script, SortedTasks) when is_list(SortedTasks) ->
     {ok, lists:foldl(fun(#{command := Command}, Acc) ->
         <<Acc/binary, Command/binary, "\n">>
-    end, <<"#!/usr/bin/env bash\n">>, SortedTasks)}.
+    end, <<"#!/usr/bin/env bash\n">>, SortedTasks)};
+
+produce_output(_ReplyMode, {error, _Reason} = Error) -> Error.
 
 -spec sort_tasks(Tasks) -> Result when
     Tasks       :: [task()],
-    Result      :: [task()].
+    Result      :: [task()] | {error, term()}.
 
 sort_tasks(Tasks) ->
     KeyedTasks = keyed(Tasks),
-    {Sorted, _} = lists:foldl(fun
-        (T, {Acc, Seen}) ->
-            sort(T, KeyedTasks, Acc, Seen)
-        end, {[], []}, Tasks),
-    lists:reverse(Sorted).
+    try
+        {Sorted, _} = lists:foldl(fun
+            (T, {Acc, Seen}) ->
+                sort(T, KeyedTasks, Acc, Seen, [])
+            end, {[], []}, Tasks),
+        lists:reverse(Sorted)
+    catch
+        throw:Descr ->
+            {error, Descr}
+    end.
 
 -spec keyed(Tasks) -> Result when
     Tasks       :: [task()],
@@ -68,27 +75,33 @@ keyed(Tasks) ->
         maps:put(Name, Task, Acc)
     end, #{}, Tasks).
 
--spec sort(Task, KeyedMap, Acc, Seen) -> Result when
-    Task        :: task(),
-    KeyedMap    :: #{task_name() => task()},
-    Acc         :: [task()],
-    Seen        :: [task_name()],
-    Result      :: {task_name(), [task_name()]}.
+-spec sort(Task, KeyedMap, Acc, Seen, CurrentPath) -> Result when
+    Task                :: task(),
+    KeyedMap            :: #{task_name() => task()},
+    Acc                 :: [task()],
+    Seen                :: [task_name()],
+    CurrentPath         :: [task_name()],
+    Result              :: {task_name(), [task_name()]}.
 
-sort(#{name := Name} = Task, KeyedMap, Acc, Seen) ->
-    case lists:member(Name, Seen) of
+sort(#{name := Name} = Task, KeyedMap, Acc, Seen, CurrentPath) ->
+    case lists:member(Name, CurrentPath) of
         true ->
-            {Acc, Seen};
+            throw({circular_dependency, Name});
         false ->
-            {NewAcc, NewSeen} = lists:foldl(fun(Dep, {DAcc, DSeen}) ->
-                case maps:get(Dep, KeyedMap, undefined) of
-                    undefined ->
-                        {DAcc, DSeen};
-                    DepTask ->
-                        sort(DepTask, KeyedMap, DAcc, DSeen)
-                end
-            end, {Acc, [Name | Seen]}, maps:get(requires, Task, [])),
-            {[maps:without([requires], Task) | NewAcc], NewSeen}
+            case lists:member(Name, Seen) of
+                true ->
+                    {Acc, Seen};
+                false ->
+                    {NewAcc, NewSeen} = lists:foldl(fun(Dep, {DAcc, DSeen}) ->
+                        case maps:get(Dep, KeyedMap, undefined) of
+                            undefined ->
+                                throw({wrong_reference_in_required, Dep});
+                            DepTask ->
+                                sort(DepTask, KeyedMap, DAcc, DSeen, [Name | CurrentPath])
+                        end
+                    end, {Acc, [Name | Seen]}, maps:get(requires, Task, [])),
+                    {[maps:without([requires], Task) | NewAcc], NewSeen}
+            end
     end.
 
 % ------------------------ end of Private helpers ------------------------------
--- test/job_processor_tests.erl
+++ test/job_processor_tests.erl
@@ -81,8 +81,42 @@ sort_tasks_test_() ->
                         )
 
                     end
+                },
+                {<<"Circular deps tests">>,
+                    fun() ->
+                        ?assertEqual({error, {circular_dependency,<<"task-6">>}}, ?TESTMODULE:sort_tasks(
+                                [
+                                    #{name => <<"task-6">>, command => <<"uptime">>, requires => [<<"task-4">>]},
+                                    #{name => <<"task-2">>, command => <<"cat /tmp/file1">>, requires => [<<"task-3">>]},
+                                    #{name => <<"task-3">>, command => <<"echo 'Hello World!' > /tmp/file1">>, requires => [<<"task-1">>]},
+                                    #{name => <<"task-1">>, command => <<"touch /tmp/file1">>},
+                                    #{name => <<"task-4">>, command => <<"rm /tmp/file1">>, requires => [<<"task-2">>, <<"task-3">>, <<"task-6">>]}
+                                ]
+                        ))
+                    end
+                },
+                {<<"Circular deps tests">>,
+                    fun() ->
+                        ?assertEqual({error, {circular_dependency,<<"task-2">>}}, ?TESTMODULE:sort_tasks(
+                                [
+                                    #{name => <<"task-1">>, command => <<"echo 'task-1'">>},
+                                    #{name => <<"task-2">>, command => <<"echo 'task-2'">>, requires => [<<"task-1">>, <<"task-3">>]},
+                                    #{name => <<"task-3">>, command => <<"echo 'task-3'">>, requires => [<<"task-2">>]}
+                                ]
+                        ))
+                    end
+                },
+                {<<"Circular deps tests">>,
+                    fun() ->
+                        ?assertEqual({error, {wrong_reference_in_required,<<"task-5">>}}, ?TESTMODULE:sort_tasks(
+                                [
+                                    #{name => <<"task-1">>, command => <<"echo 'task-1'">>},
+                                    #{name => <<"task-2">>, command => <<"echo 'task-2'">>, requires => [<<"task-1">>, <<"task-3">>]},
+                                    #{name => <<"task-3">>, command => <<"echo 'task-3'">>, requires => [<<"task-5">>]}
+                                ]
+                        ))
+                    end
                 }
-
             ]
         }
     }.
@@ -99,6 +133,14 @@ process_tasks_test_() ->
                         ExpectedJsonBashScript = <<"#!/usr/bin/env bash\ntouch /tmp/file1\necho 'Hello World!' > /tmp/file1\ncat /tmp/file1\nrm /tmp/file1\n">>,
                         ?assertEqual({ok, ExpectedJson}, ?TESTMODULE:process_tasks(InputJson, sorted)),
                         ?assertEqual({ok, ExpectedJsonBashScript}, ?TESTMODULE:process_tasks(InputJson, bash_script))
+                    end
+                },
+                {<<"Circular dependencies">>,
+                    fun() ->
+                        InputJson = <<"{\"tasks\":[{\"command\":\"touch /tmp/file1\",\"name\":\"task-1\"},{\"command\":\"cat /tmp/file1\",\"name\":\"task-2\",\"requires\":[\"task-2\"]},{\"command\":\"echo 'Hello World!' > /tmp/file1\",\"name\":\"task-3\",\"requires\":[\"task-1\"]},{\"command\":\"rm /tmp/file1\",\"name\":\"task-4\",\"requires\":[\"task-2\",\"task-3\"]}]}">>,
+                        ExpectedError = {error, {circular_dependency, <<"task-2">>}},
+                        ?assertEqual(ExpectedError, ?TESTMODULE:process_tasks(InputJson, sorted)),
+                        ?assertEqual(ExpectedError, ?TESTMODULE:process_tasks(InputJson, bash_script))
                     end
                 }
             ]
